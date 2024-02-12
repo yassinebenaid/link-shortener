c9af0e3904fd2c3e0f7a666102eb926caf60e827
yassinebenaid <yassinebenaide3@gmail.com>
2024-02-12 15:59:37 +0100

    added clicks history graph tests

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -19,7 +19,7 @@ class HomeTest extends TestCase
     {
         $user = User::factory()->create();
         $links = Link::factory(10)->create(['user_id' => $user->id]);
-        Click::factory(20)->create(['model_type' => Link::class, 'model_id' => $links->first()->id]);
+        Click::factory(20)->create(['model_type' => Link::class, 'model_id' => $links->first()->id, 'created_at' => now()]);
 
         $this->actingAs($user);
         $this->get(route('home'))
@@ -27,6 +27,9 @@ class HomeTest extends TestCase
             ->assertInertia(fn (AssertableInertia $page) => $page
                 ->component('Home/Index')
                 ->where('totalLinks', 10)
-                ->where('totalClicks', 20));
+                ->where('totalClicks', 20)
+                ->count('clicksHistory', 30)
+                ->where('clicksHistory.29.count', 20)
+                ->where('clicksHistory.28.count', 0)); // make sure dates are correct
     }
 }
